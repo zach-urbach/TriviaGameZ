7535ff20e9952f6984cd63d49825c17fa980edfc
Zach Urbach <urbach-zach@gmail.com>
2014-07-02 09:06:28 -0600

    my trivia game

---
+++ TriviaGameZ/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaGameZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Trivia game function goes here
+        }
+
+
+        //This functions gets the full list of trivia questions from the Trivia.txt document
+        static List<Trivia> GetTriviaList()
+        {
+            //Get Contents from the file.  Remove the special char "\r".  Split on each line.  Convert to a list.
+            List<string> contents = File.ReadAllText("trivia.txt").Replace("\r", "").Split('\n').ToList();
+
+            //Each item in list "contents" is now one line of the Trivia.txt document.
+
+            //make a new list to return all trivia questions
+            List<Trivia> returnList = new List<Trivia>();
+            // TODO: go through each line in contents of the trivia file and make a trivia object.
+            //       add it to our return list.
+
+            //Return the full list of trivia questions
+            return returnList;
+        }
+    }
+}
---
+++ TriviaGameZ/Trivia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaGameZ
+{
+    class Trivia
+    {
+        //TODO: Fill out the Trivia Object
+
+    }
+}
